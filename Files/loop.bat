3d8bf027e64b703ebac2af9a349ce12af0cdd35f
BurtonFernsby1990 <87943910+BurtonFernsby1990@users.noreply.github.com>
2021-07-25 20:28:53 +0200

    UPDATE

--- Files/loop.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-tasklist | find /i "ngrok.exe" >Nul && goto check || echo "NGROK Services is not Running." & ping 127.0.0.1 >Nul & ping 127.0.0.1 >Nul & exit
-:check
-ping 127.0.0.1 > nul
-cls
-goto check
