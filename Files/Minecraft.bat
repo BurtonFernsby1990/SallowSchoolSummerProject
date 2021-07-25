41e2ad008a7c96ebf615838f4c6ebc573191a5e2
BurtonFernsby1990 <87943910+BurtonFernsby1990@users.noreply.github.com>
2021-07-25 23:00:57 +0200

    Update Minecraft.bat

--- Files/Minecraft.bat
+++ Files/Minecraft.bat
@@ -4,3 +4,4 @@ C:\Users\administrator\Desktop\jre-8u301-windows-x64.exe /s
 del "C:\Users\administrator\Desktop\jre-8u301-windows-x64.exe"
 mkdir "C:\Users\administrator\Desktop\Minecraft"
 curl -o "C:\Users\administrator\Desktop\Minecraft\mohist-1.16.5.jar" https://serverjars.com/api/fetchJar/mohist/1.16.5
+del "C:\Users\Public\Desktop\Minecraft.bat"
